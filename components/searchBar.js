baad2e9104bf981503444ffdc89b8bae3ca1c3c2
Vitalii Kalinin <kalinin.vitali@gmail.com>
2023-01-07 10:17:25 -0800

    chore: add comments search bar

--- components/searchBar.js
+++ components/searchBar.js
@@ -6,30 +6,59 @@ class SearchBar extends HTMLElement {
     // Добавляем shadow DOM tree
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
     this.attachShadow({ mode: "open" });
+    // <my-search-bar>
+    // #shadow-root(open)
+    // </my-search-bar>
   }
 
   connectedCallback() {
     // Создаем инпут варинат 1
     const textInput = document.createElement("input");
+    // <input />
     textInput.setAttribute("type", "text");
+    // <input type="text" />
     textInput.setAttribute("name", "text");
+    // <input type="text" name="text" />
     textInput.setAttribute("class", "search");
+    // <input type="text" name="text" class="search" />
     textInput.setAttribute("placeholder", "Поиск");
+    // <input type="text" name="text" class="search" placeholder="Посик"/>
 
     // Создаем инпут варинат 2
     const submitBtn = document.createElement("input");
+    // <input />
     submitBtn.type = "submit";
+    // <input type="submit"/>
     submitBtn.name = "submit";
+    // <input type="submit" name="submit"/>
     submitBtn.className = "submit";
+    // <input type="submit" name="submit" class="submit"/>
     submitBtn.value = "Найти";
+    // <input type="submit" name="submit" class="submit" value="Найти"/>
 
     // Создаем форму и прикрепляем инпуты внутрь
     const form = document.createElement("form");
+    // <form>
+    // </form>
     form.appendChild(textInput);
+    // <form>
+    //   <input type="text" name="text" class="search" placeholder="Посик"/>
+    // </form>
     form.appendChild(submitBtn);
+    // <form>
+    //   <input type="text" name="text" class="search" placeholder="Посик"/>
+    //   <input type="submit" name="submit" class="submit" value="Найти"/>
+    // </form>
 
     // Приклепляем форму с инпутами на ShadowRoot
     this.shadowRoot.append(form);
+    // <my-search-bar>
+    // #shadow-root(open)
+    //   <form>
+    //     <input type="text" name="text" class="search" placeholder="Посик"/>
+    //     <input type="submit" name="submit" class="submit" value="Найти"/>
+    //   </form>
+    // </my-search-bar>
   }
 }
 // Регистрируемый кастомный тэг <my-search-bar></my-search-bar>
